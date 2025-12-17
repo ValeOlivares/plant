b19593dd4845628548e0ea1b6d78c0e541027f9a
ValeOlivares <valentinaolivars85@gmail.com>
2025-12-17 20:38:50 -0300

    feat: add properties

--- src/main/java/net/vale/plant/controller/HomeController.java
+++ src/main/java/net/vale/plant/controller/HomeController.java
@@ -12,8 +12,18 @@ public class HomeController {
 
   @GetMapping("/")
   public String showHome(Model model) {
-    model.addAttribute("message", "Welcome to the Plant Application");
-    model.addAttribute("date", new Date());
+    // model.addAttribute("message", "Welcome to the Plant Application");
+    // model.addAttribute("date", new Date());
+    String name = "Ipomea";
+    Date pubDate = new Date();
+    double price = 15.99;
+    boolean available = true;
+
+    model.addAttribute("name", name);
+    model.addAttribute("pubDate", pubDate);
+    model.addAttribute("price", price);
+    model.addAttribute("available", available);
+
     return "home";
   }
 
